41bdd2c06dd659765c2b4d3b602628ef01d2aaa7
Jim Balhoff <jim@balhoff.org>
2023-08-10 12:44:52 -0400

    Add general documents. Don't output "searchable" fields.

--- src/main/scala/org/geneontology/golr/Main.scala
+++ src/main/scala/org/geneontology/golr/Main.scala
@@ -11,6 +11,7 @@ import sttp.client3.ziojson._
 import sttp.model.Uri
 import zio._
 import zio.json._
+import zio.json.internal.Write
 import zio.stream._
 
 import java.io.File
@@ -27,7 +28,14 @@ object Main extends ZIOAppDefault {
   val oioHasNamespace = ResourceFactory.createProperty(s"$oio#hasOBONamespace")
   val definition = ResourceFactory.createProperty("http://purl.obolibrary.org/obo/IAO_0000115")
 
-  implicit val recordJsonEncoder: JsonEncoder[Record] = DeriveJsonEncoder.gen[Record]
+  implicit val ontologyClassJsonEncoder: JsonEncoder[OntologyClassRecord] = DeriveJsonEncoder.gen[OntologyClassRecord]
+  implicit val generalJsonEncoder: JsonEncoder[GeneralRecord] = DeriveJsonEncoder.gen[GeneralRecord]
+  implicit val recordJsonEncoder: JsonEncoder[Record] = new JsonEncoder[Record] {
+    override def unsafeEncode(a: Record, indent: Option[RuntimeFlags], out: Write): Unit = a match {
+      case ocr: OntologyClassRecord => ontologyClassJsonEncoder.unsafeEncode(ocr, indent, out)
+      case gr: GeneralRecord        => generalJsonEncoder.unsafeEncode(gr, indent, out)
+    }
+  }
 
   override def run =
     for {
@@ -42,11 +50,16 @@ object Main extends ZIOAppDefault {
         .mapZIOParUnordered(20) { term =>
           indexTerm(term, dataset)
         }
-        .collect { case Some(record) => record }
+        .collect { case Some(record) => ZStream.fromIterable(List(record._1, record._2)) }
+        .flatten
         .grouped(1000)
         .map { records =>
           basicRequest.post(solrEndpoint)
             .body(records)
+        }
+        .map { req =>
+          println(req)
+          req
         }
         .mapZIOParUnordered(4) { request =>
           request.send(httpClient)
@@ -85,14 +98,18 @@ object Main extends ZIOAppDefault {
     runSelect(query, dataset)(_.getResource("term"))
   }
 
-  def indexTerm(term: Resource, dataset: Dataset): Task[Option[Record]] =
+  def indexTerm(term: Resource, dataset: Dataset): Task[Option[(GeneralRecord, OntologyClassRecord)]] =
     for {
       maybeProps <- getProperties(term, dataset)
       record <- ZIO.foreach(maybeProps) { props =>
         for {
           synonyms <- getSynonyms(term, dataset)
           superclasses <- getSuperclasses(term, dataset)
-        } yield Record.create(props, synonyms, superclasses)
+        } yield {
+          val curie = compact(term.getURI)
+          (GeneralRecord.create(curie, props, synonyms),
+            OntologyClassRecord.create(curie, props, synonyms, superclasses))
+        }
       }
     } yield record
 
@@ -102,7 +119,9 @@ object Main extends ZIOAppDefault {
         SELECT DISTINCT ?label ?def ?namespace ?obsolete
         WHERE {
           $term ${RDFS.label} ?label .
-          $term $definition ?def .
+          OPTIONAL {
+            $term $definition ?def .
+          }
           OPTIONAL {
             $term $oioHasNamespace ?namespace .
           }
@@ -117,7 +136,7 @@ object Main extends ZIOAppDefault {
       Term(
         iri = term.getURI,
         label = qs.getLiteral("label").getLexicalForm,
-        definition = qs.getLiteral("def").getLexicalForm,
+        definition = Option(qs.getLiteral("def")).map(_.getLexicalForm),
         namespace = Option(qs.getLiteral("namespace")).map(_.getLexicalForm),
         obsolete = qs.getLiteral("obsolete").getBoolean
       )
@@ -155,47 +174,63 @@ object Main extends ZIOAppDefault {
 
   final case class Labeled(iri: String, label: String)
 
-  final case class Term(iri: String, label: String, definition: String, namespace: Option[String], obsolete: Boolean)
+  final case class Term(iri: String, label: String, definition: Option[String], namespace: Option[String], obsolete: Boolean)
 
-  final case class Record(
-                           document_category: String,
-                           id: String,
-                           annotation_class: String,
-                           annotation_class_label: String,
-                           annotation_class_label_searchable: String,
-                           description: String,
-                           description_searchable: String,
-                           source: Option[String],
-                           idspace: String,
-                           is_obsolete: Boolean,
-                           synonym: List[String],
-                           synonym_searchable: List[String],
-                           isa_closure: List[String],
-                           isa_closure_label: List[String],
-                           isa_closure_label_searchable: List[String]
-                         )
+  sealed trait Record extends Product with Serializable
 
-  object Record {
+  final case class OntologyClassRecord(
+                                        document_category: String,
+                                        id: String,
+                                        annotation_class: String,
+                                        annotation_class_label: String,
+                                        description: Option[String],
+                                        source: Option[String],
+                                        idspace: String,
+                                        is_obsolete: Boolean,
+                                        synonym: List[String],
+                                        isa_closure: List[String],
+                                        isa_closure_label: List[String]
+                                      ) extends Record
 
-    def create(term: Term, synonyms: List[String], superclasses: List[Labeled]): Record = {
-      val curie = compact(term.iri)
-      val isaClosureLabels = superclasses.map(sup => compact(sup.label))
-      Record(
+  object OntologyClassRecord {
+
+    def create(curie: String, term: Term, synonyms: List[String], superclasses: List[Labeled]): OntologyClassRecord = {
+      OntologyClassRecord(
         document_category = "ontology_class",
         id = curie,
         annotation_class = curie,
         annotation_class_label = term.label,
-        annotation_class_label_searchable = term.label,
         description = term.definition,
-        description_searchable = term.definition,
         source = term.namespace,
         idspace = curie.split(":", 2).head,
         is_obsolete = term.obsolete,
         synonym = synonyms,
-        synonym_searchable = synonyms,
         isa_closure = superclasses.map(sup => compact(sup.iri)),
-        isa_closure_label = isaClosureLabels,
-        isa_closure_label_searchable = isaClosureLabels
+        isa_closure_label = superclasses.map(sup => compact(sup.label))
+      )
+    }
+
+  }
+
+  final case class GeneralRecord(
+                                  document_category: String,
+                                  id: String,
+                                  entity: String,
+                                  entity_label: String,
+                                  category: String,
+                                  general_blob: String,
+                                ) extends Record
+
+  object GeneralRecord {
+
+    def create(curie: String, term: Term, synonyms: List[String]): GeneralRecord = {
+      GeneralRecord(
+        document_category = "general",
+        id = s"general_ontology_class_$curie", // in the official golr-loader, various categories are used here
+        entity = curie,
+        entity_label = term.label,
+        category = "ontology_class", // in the official golr-loader, various categories are used here
+        general_blob = s"$curie ${term.definition.getOrElse("")} ${synonyms.mkString(" ")}" // not exactly the same as the official golr-loader
       )
     }
 
